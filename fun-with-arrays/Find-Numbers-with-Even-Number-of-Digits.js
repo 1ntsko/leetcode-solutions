56b98d58653f29e5a0f01cf465ea3b60a41a74ff
1ntsko <giorgilasha3@gmail.com>
2022-04-21 18:59:29 +0400

    final

--- fun-with-arrays/Find-Numbers-with-Even-Number-of-Digits.js
+++ fun-with-arrays/Find-Numbers-with-Even-Number-of-Digits.js
@@ -10,4 +10,14 @@ Explanation:
 Therefore only 12 and 7896 contain an even number of digits.
 */
 
-var findNumbers = function (nums) {};
+var findNumbers = function (nums) {
+    let count = 0;
+
+    nums.forEach((num) => {
+        if (num.toString().length % 2 === 0) count++;
+    });
+
+    return count;
+};
+
+console.log(findNumbers([12, 345, 2, 6, 7896]));
